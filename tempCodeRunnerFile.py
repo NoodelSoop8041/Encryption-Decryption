a98dc355e6d8296156ed334e71992467baca244f
NoodelSoop8041 <leshoue2006@gmail.com>
2024-12-05 11:37:24 -0800

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-def mod_inverse(multiple):
-    """Finds the inverse of the encryption multiple under mod 26"""
-
-    for x in range(1, 26):
-        if (multiple*x) % 26 == 1:
-            return x
-    return None
