d5e4661a3a3582bd1e1e26475f210e13dd9ef3b2
NoodelSoop8041 <noodelsoop8041@gmail.com>
2024-12-10 13:37:15 -0800

    Submission Code V1
    Finished initial version of Vigenere Cypher.

--- VigenereCypher.py
+++ VigenereCypher.py
@@ -1,33 +1,53 @@
+#This is our key, simplified to include only lowercase letters
+key = "mrandmrsdursleyofnumberfourprivetdrivewereproudtosaythattheywereperfectlynormalthankyouverymuch"
+key_list = []
+    
+for char in key:
+    key_list.append(ord(char) - 97)
+
+text = input("Please enter your message:" )
+
 def vigenere_encrypt(text):
     "Encrypts using the Vigenere Cipher"
 
-    #This is our key, simplified to include only lowercase letters
-    key = "mrandmrsdursleyofnumberfourprivetdrivewereproudtosaythattheywereperfectlynormalthankyouverymuch"
-    list = []
-    
-    for char in key:
-        list.append(ord(char) - 96)
-
     message = ""
-    for char in text:
-        if not char.isalpha():
-            message += char
+
+    for i in range(len(text)):
+        char = text[i]
         if char.islower():
-            cipher_char = (ord(char) - 96)
-            message += chr(cipher_char + list[key])
+            cipher_char = (ord(char) - 97)
+            message += chr((cipher_char + key_list[i])%26 + 97)
+        elif char.isupper():
+            cipher_char = (ord(char) - 65)
+            message += chr((cipher_char + key_list[i])%26 + 65)
         else:
-            cipher_char = (ord(char) - 64)
+            message += char
 
     return message
 
 
+def vigenere_decrypt(text):
+    "Decrypts using Vigenere Cypher"
 
-text = input("Please enter your message:" )
 
-print(vigenere_encrypt(text))
+    message = ""
 
-e_d = input("Would you like to encrypt or decrypt?" )
+    for i in range(len(text)):
+        char = text[i]
+        if char.islower():
+            cipher_char = (ord(char) - 97)
+            message += chr((cipher_char - key_list[i])%26 + 97)
+        elif char.isupper():
+            cipher_char = (ord(char) - 65)
+            message += chr((cipher_char - key_list[i])%26 + 65)
+        else:
+            message += char
+
+    return message
+
+e_d = input("Would you like to Encrypt or Decrypt a message?")
+
 if e_d.lower() == "encrypt":
-    print(vigenere_encrypt(text))
-if e_d.lower() == "decrypt":
-    print(vigenere_decrypt(text))
+    print("Message:", vigenere_encrypt(text))
+elif e_d.lower() == "decrypt":
+    print("Message:", vigenere_decrypt(text))
